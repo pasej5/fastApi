13193a01c2a4e115c9d2b8a30993ba3b57dccc51
Jealous Matsikachando <pasej5@gmail.com>
2025-02-18 10:03:00 +0200

    Routers added

--- app/main.py
+++ app/main.py
@@ -115,7 +115,7 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db.refresh(new_user)
     return new_user
 
-@app.get('/users/{id}')
+@app.get('/users/{id}', response_model=schemas.UserOut)
 def get_user(id: int, db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.id == id).first()
     
