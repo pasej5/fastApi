76c9e0aa48fba61f565f81360787ccf2f86e4ba4
Jealous Matsikachando <pasej5@gmail.com>
2025-02-18 10:07:03 +0200

    Moving all users and posts to their routs

---
+++ app/routers/post.py
@@ -0,0 +1,54 @@
+
+
+@app.get("/posts", response_model=List[schemas.Post])
+def get_posts(db: Session = Depends(get_db)):
+    posts = db.query(models.Post).all()    
+    # cursor.execute("""SELECT * FROM posts """)
+    # posts = cursor.fetchall()
+    return posts
+
+@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
+def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
+    new_post = models.Post(**post.dict())
+    
+    # cursor.execute("""INSERT INTO posts (title, content) VALUES (%s, %s) RETURNING *""", (posts.title, posts.content))
+    # new_post = cursor.fetchone()
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+    return new_post
+
+@app.get("/posts/{id}", response_model=schemas.Post)
+def get_post(id: int, db: Session = Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id == id).first()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with {id} was not found")
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {"message": f"post with {id} was not found"} 
+        
+    return post
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int, db: Session = Depends(get_db)):
+    # deleting a post is mainly trying to figure out which dictionary from my post array sould we delete.
+    # first find the index with the required id in you array
+    # my_posts.pop(index)
+    post = db.query(models.Post).filter(models.Post.id == id)
+    
+    
+    if post.first() == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {id} does not exis")
+    post.delete(synchronize_session=False)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+    # we send this because we dont want to send any data back since we deleted the code Response(status_code=status.HTTP_204_NO_CONTENT)
+    
+
+@app.put("/posts/{id}", response_model=schemas.Post)
+def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db)):
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post = post_query.first()
+    if post_query == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {id} does not exis")
+    post_query.update(post.dict(),synchronize_session=False)
+    return post_query.first()
