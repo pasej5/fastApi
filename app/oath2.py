9f367803065d9e8a1343caf7c2baa90a7c50c5ad
Jealous Matsikachando <pasej5@gmail.com>
2025-02-20 13:27:09 +0200

    function to verify the token

--- app/oath2.py
+++ app/oath2.py
@@ -1,5 +1,6 @@
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
+from .import schemas
 
 SECRET_KEY = "09jsvjgjbvsjbjhjheuyhueq878567480j0k098jgsfdfc24fnsvjsvnsg5gghwhyhwdg6hjfgjhfw78bjfbwjnh8"
 ALGORITHM = "HS256"
@@ -12,3 +13,17 @@ def create_access_token(data: dict):
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     
     return encoded_jwt
+
+def verify_access_token(token: str, credentials_exception):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+        id: str = payload.get("users_id")
+        
+        if id is None:
+            raise credentials_exception
+        token_data = schemas.TokenData(id=id)
+    except JWTError:
+        raise credentials_exception
+    
+def get_current_user
+        
