84a3a85e2a3115e8840266a7522697e6bcfa06be
Jealous Matsikachando <pasej5@gmail.com>
2025-03-08 23:41:16 +0200

    alembic add content column

---
+++ alembic/versions/9c1c6bb1bd38_add_content_column_to_table.py
@@ -0,0 +1,28 @@
+"""add content column to table
+
+Revision ID: 9c1c6bb1bd38
+Revises: 4541ffd260e5
+Create Date: 2025-03-08 23:38:10.269750
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '9c1c6bb1bd38'
+down_revision: Union[str, None] = '4541ffd260e5'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.add_column('post', sa.Column('content', sa.String(), nullable=False))
+    
+    pass
+
+
+def downgrade() -> None:
+    pass
