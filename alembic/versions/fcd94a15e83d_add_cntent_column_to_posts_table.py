c06a42546b3c85dc48a0635ba28090de7d8d63ed
Jealous Matsikachando <pasej5@gmail.com>
2025-05-03 01:00:55 +0200

    add content to column table

---
+++ alembic/versions/fcd94a15e83d_add_cntent_column_to_posts_table.py
@@ -0,0 +1,28 @@
+"""add cntent column to posts table
+
+Revision ID: fcd94a15e83d
+Revises: 2da7a70708a0
+Create Date: 2025-05-03 01:00:20.486526
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'fcd94a15e83d'
+down_revision: Union[str, None] = '2da7a70708a0'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
